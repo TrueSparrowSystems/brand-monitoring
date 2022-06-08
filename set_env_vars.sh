05ae80dab1fbd36d2b8d1eecfeb12b21e2499591
V_R_Dighe <vaibhav@moxie.xyz>
2022-06-08 15:45:05 +0530

    Lib for sentiment analysis using google NLP.

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -4,3 +4,6 @@
 export BM_AWS_COMPREHEND_REGION='us-east-1';
 export BM_AWS_COMPREHEND_ACCESS_KEY_ID='AKIAT7WAUYD3SJOS26FU';
 export BM_AWS_COMPREHEND_SECRET_ACCESS_KEY='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';
+
+# Google Application Credentials
+export GOOGLE_APPLICATION_CREDENTIALS="./test/plg-works-poc-11306a145e0e.json"
