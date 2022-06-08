485eaf39e86e5a861b6673efeb5365d6bab52ff9
kedarchandrayan <kedarchandrayan@gmail.com>
2022-06-08 13:15:13 +0530

    AWS comprehend.

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 # AWS Comprehend
+export BM_AWS_COMPREHEND_REGION='us-east-1';
+export BM_AWS_COMPREHEND_ACCESS_KEY_ID='AKIAT7WAUYD3SJOS26FU';
+export BM_AWS_COMPREHEND_SECRET_ACCESS_KEY='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';
